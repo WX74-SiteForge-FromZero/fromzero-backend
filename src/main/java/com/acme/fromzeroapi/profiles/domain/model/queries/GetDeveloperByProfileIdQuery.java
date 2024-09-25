05c900303cd22c42bdbb1797f6bfc8c70c2ae2b6
JorgeQuilla <u20211b197@upc.edu.pe>
2024-09-25 18:54:22 -0500

    initial commit

---
+++ src/main/java/com/acme/fromzeroapi/profiles/domain/model/queries/GetDeveloperByProfileIdQuery.java
@@ -0,0 +1,6 @@
+package com.acme.fromzeroapi.profiles.domain.model.queries;
+
+import com.acme.fromzeroapi.profiles.domain.model.valueobjects.ProfileId;
+
+public record GetDeveloperByProfileIdQuery(String profileId) {
+}
---
+++ src/test/java/com/acme/fromzeroapi/WebMasterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.fromzeroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMasterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
