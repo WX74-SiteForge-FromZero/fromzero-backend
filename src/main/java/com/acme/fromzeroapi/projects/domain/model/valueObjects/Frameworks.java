f81b1e8b75100d0da20c300350a7742418cfad72
JorgeQuilla <u20211b197@upc.edu.pe>
2024-11-15 18:41:54 -0500

    feat(projects): languages and frameworks added

--- src/main/java/com/acme/fromzeroapi/projects/domain/model/valueObjects/Frameworks.java
+++ src/main/java/com/acme/fromzeroapi/projects/domain/model/valueObjects/Frameworks.java
@@ -3,5 +3,24 @@ package com.acme.fromzeroapi.projects.domain.model.valueObjects;
 public enum Frameworks {
     Vue_Js,
     Angular,
-    React
+    React,
+    Bootstrap,
+    Tailwind_CSS,
+    Express,
+    Django,
+    Flask,
+    Laravel,
+    Svelte,
+    Spring_Boot,
+    ASP_NET,
+    Flutter,
+    Jetpack_Compose,
+    React_Native,
+    Xamarin,
+    Tkinter,
+    Qt,
+    Swing,
+    JavaFX,
+    WPF,
+    Electron
 }
